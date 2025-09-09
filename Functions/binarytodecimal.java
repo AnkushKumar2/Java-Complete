febf0fe46d403baeaae98dba6f0c5a1a8483dca9
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-09-09 23:59:21 +0530

    functions commit

--- Functions/binarytodecimal.java
+++ Functions/binarytodecimal.java
@@ -1,9 +1,28 @@
 import java.util.*;
 
 public class binarytodecimal {
+
+
+
+    public static int BinarytoDecimal(int n){
+        int ans=0;
+        int rem,mul=1;
+        while(n!=0){
+            n%2=rem;
+            n/=2;
+            ans=ans*mul+rem;
+            mul*=10;
+
+        }
+        return ans;
+    }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.print("heyyy");
+        // System.out.print("heyyy");
+        int num;
+        num=sc.nextInt();
+        System.out.println(BinarytoDecimal(num));
+
     }
     
 }
