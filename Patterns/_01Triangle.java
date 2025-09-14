fb58dca82269fc554d42ebc76c6e47ef6e5f5a18
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-09-14 17:47:06 +0530

    patterns

--- Patterns/_01Triangle.java
+++ Patterns/_01Triangle.java
@@ -2,12 +2,16 @@ import java.util.*;
 public class _01Triangle {
 
     public static void _01Triangle_(int totrow,int totcol){
-        int num=1;
+        // int num=1;
         for(int i=1;i<=totrow;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(num);
-                System.out.print(" ");
-                num=(num==1)?0:1;
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            
             }
             System.out.println();
         }
