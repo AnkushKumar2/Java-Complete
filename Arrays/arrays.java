58ba60691f76a8459afe86614e68e250498ba1fa
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-09-16 11:41:38 +0530

    arrays

---
+++ Arrays/arrays.java
@@ -0,0 +1,16 @@
+// package Arrays;
+import java.util.*;
+
+public class arrays {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        // System.out.print("eyyyy");
+        // int array1[]= new int[50]; 
+        int marks[]=new int[100];
+        for(int i=0;i<5;i++){
+            marks[i]=sc.nextInt();
+        }
+        System.out.print(marks[4]);
+    }
+    
+}
