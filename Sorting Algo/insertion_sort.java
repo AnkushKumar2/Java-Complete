949f3c761e46a0c9671fe811946c44986047f1e8
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-10-03 17:33:39 +0530

    sorting commit

---
+++ Sorting Algo/insertion_sort.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class insertion_sort {
+
+    public static void Insertion_sort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            for(int j=i;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+
+
+
+
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n=sc.nextInt();
+        int arr1[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr1[i]=sc.nextInt();
+        }
+        Insertion_sort(arr1);
+
+
+
+    }
+    
+}
