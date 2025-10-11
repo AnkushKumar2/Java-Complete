7d66eb5f22f3ccbfae5cd1773802f8c95fc5627d
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-10-11 23:36:30 +0530

    arrays commit

--- Arrays/binary_search.java
+++ Arrays/binary_search.java
@@ -1,8 +1,42 @@
 import java.util.*;
 
 public class binary_search {
+
+
+    public static int binarySearch(int num[],int key){
+        int start=0, end=num.length-1; 
+
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(num[mid]==key){
+                return mid;
+            }
+            else if(num[mid]<key){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            
+
+        }
+        return -1;
+
+    }
     public static void main(String args[]){
         Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the size of array: ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        System.out.print("Enter key: ");
+        int key=sc.nextInt();
+
+        System.out.println(binarySearch(arr, key));
         
           
 
