c5e2d6ccbc92ad8b2f35efebfb9e299194431384
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-11-09 23:50:23 +0530

    arrays

---
+++ Arrays/max_subarray_sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class max_subarray_sum {
+
+    
+    public static void main(String args[]);
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter the size of array: ");
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    Arrays.sort(arr);
+
+    
+}
