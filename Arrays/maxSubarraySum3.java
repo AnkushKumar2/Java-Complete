07fa28ff30a5393d4d920c073befa227d03e9ac9
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-11-11 00:32:29 +0530

    kadanes

---
+++ Arrays/maxSubarraySum3.java
@@ -0,0 +1,32 @@
+// KADANE'S ALGORITHM APPROACH of TIME COMPLEXITY = O(N)
+
+import java.util.*;
+
+
+public class maxSubarraySum3 {
+
+    public static void kadanes(int num[]){
+        int maxSum=Integer.MIN_VALUE;
+        int currSum=0;
+        for(int i=0;i<num.length;i++){
+            currSum=currSum+num[i];
+            if(currSum<0){
+                currSum=0;
+            }
+            maxSum=Math.max(currSum, maxSum);
+        }
+        System.out.println("Max subarray Sum: "+maxSum);
+    }
+    public static void main(String args[]){
+         Scanner sc= new Scanner(System.in);
+    System.out.print("Enter the size of array: ");
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    kadanes(arr);
+
+    }
+    
+}
