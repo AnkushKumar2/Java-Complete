e376446db68e00d5f04b127eac896e5ac7a5503d
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-11-13 20:12:31 +0530

    arrays

---
+++ Arrays/assigmentQ5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class assigmentQ5 {
+    public static int Triplet(int num[]){
+        
+    }
+    public static void main(String args[]){
+         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+    }
+    
+}
