a17cdcc7d4de0b80e39a3d9d4dd0f453637ee20b
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-11-16 13:50:31 +0530

    2d arrays

--- 2D Arrays/DiagonalOptimized.java
+++ 2D Arrays/DiagonalOptimized.java
@@ -1,28 +1,19 @@
-//Time complexity is O(n^2)
-
+//Time Complexity is:O(n)
 
 import java.util.*;
 
-public class diagonal {
-    public static int DiagonalSum(int matrix[][]){
+public class DiagonalOptimized {
+    public static int DiagonalSum_Optimized(int matrix[][]){
         int sum=0;
-        int primarySum=0;
-        int secondarySum=0;
-        
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(i==j){
-                    primarySum+=matrix[i][j];
-                }
-                else if(i+j==matrix.length-1){
-                    secondarySum+=matrix[i][j];
-                }
+            sum+=matrix[i][i];
+            if(i!=matrix.length-1-i){
+                sum+=matrix[i][matrix.length-1-i];
             }
         }
-        sum=primarySum+secondarySum;
-    
         return sum;
     }
+
     public static void main(String args[]){
          Scanner sc=new Scanner(System.in);
          System.out.println("Enter no. of rows: ");
@@ -34,8 +25,9 @@ public class diagonal {
             for(int j=0;j<n;j++){
                 matrix[i][j]=sc.nextInt();
             }
-        }
-        System.out.print("Both Diagonals Sum will be: "+DiagonalSum(matrix));
+
     }
+    System.out.println("Sum of Diagonals will be: "+DiagonalSum_Optimized(matrix));
     
+}
 }
