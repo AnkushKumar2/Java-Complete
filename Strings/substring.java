5444a9a8965d4633327f4b5cdd0be0438f4d34c0
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-11-17 18:03:19 +0530

    strings

---
+++ Strings/substring.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class substring {
+    public static String SubString(String str,int si,int ei){
+        String substr="";
+        for(int i=si;i<ei;i++){
+            substr+=str.charAt(i);
+        }
+        return substr;
+
+
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int si=sc.nextInt();
+        int ei=sc.nextInt();
+        System.out.println(SubString(str, si, ei));
+        System.out.println(str.substring(si, ei));
+
+    }
+    
+}
