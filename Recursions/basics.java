3874b92897cd687385352f2aae84e2ad195a9e1e
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-11-24 00:31:05 +0530

    recursion basics

--- Recursions/basics.java
+++ Recursions/basics.java
@@ -9,10 +9,31 @@ public class basics {
         System.out.print(n+" ");
         printDec(n-1);
     }
+
+    public static void printInc(int  n){
+        if(n==1){
+            System.out.print(n+" ");
+            return ;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+
+    public static int printFact(int n){
+        if(n==0){
+            
+            return 1;
+        }
+        int Fnm1=printFact(n-1);
+        int Fn=n*printFact(n-1);
+        return Fn;
+    }
     public static void main( String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         printDec(n);
+        printInc(n);
+        System.out.println(printFact(n));
     }
     
 }
