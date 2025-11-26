39a8941a234a0f26e19a70bd8a6f1dd4a23ba5d8
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-11-26 22:33:17 +0530

    recursions basic

--- Recursions/arraysRecur.java
+++ Recursions/arraysRecur.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class arraysRecur {
     //check array is sorted or not
-    //TC=O(n) and SC=O(n)
+    //TC=O(n) and SC=O(n) 
     public static boolean isSorted(int arr[],int i){
         if(i==arr.length-1){
             return true;
@@ -13,10 +13,22 @@ public class arraysRecur {
         return isSorted(arr, i+1);
 
     }
+    //first occurence of an element in an array
+    public static int firstOccurence(int arr[],int key,int i){
+        if(i==arr.length){
+            return -1;
+        }
+        if(arr[i]==key){
+            return i;
+        }
+        return firstOccurence(arr, key, i+1);
+    }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        int arr[]={1,2,3,4,5};
+        int arr[]={1,2,3,4,5,3,5};
+        int key=sc.nextInt();
         System.out.println(isSorted(arr, 0));
+        System.out.println(firstOccurence(arr, key, 0));
 
 
     }
