c9bac11c73cb6ca551ab0c1cdc486187fbe2ad54
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-11-28 16:35:15 +0530

    recursion basics

--- Recursions/XpowerN.java
+++ Recursions/XpowerN.java
@@ -8,11 +8,24 @@ public class XpowerN {
         }
         return x*pow(x, n-1);
     }
+    //Optimized code
+    public static int pow2(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        int f1=pow2(x, n/2);
+        int f2=f1*f1;
+        if(n%2!=0){
+            f2=x*f2;
+        }
+        return f2;
+    }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
         int n=sc.nextInt();
         System.out.println(pow(x, n));
+        System.out.println(pow2(x, n));
     }
     
 }
