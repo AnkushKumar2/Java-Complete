672829b58f3cbe72351ebc5e3f3d2d486f053cb6
AnkushKumar2 <ankushgupta97412@gmail.com>
2025-12-31 11:58:30 +0530

    arraylists

--- ArrayLists/findmax.java
+++ ArrayLists/findmax.java
@@ -11,9 +11,10 @@ public class findmax {
         list.add(6);
         int max=Integer.MIN_VALUE;
         for(int i=0;i<list.size();i++){
-            if(max<list.get(i)){
-                max=list.get(i);
-            }
+            // if(max<list.get(i)){
+            //     max=list.get(i);
+            // }
+            max=Math.max(max,list.get(i));
             
         }
         System.out.println(max);
